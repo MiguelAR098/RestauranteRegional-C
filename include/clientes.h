dbaf4e225e36a602097533e470ddba0dc23ec183
Miguel Arcanjo <miguelarcanjomartinsilva@gmail.com>
2026-06-13 17:58:03 -0300

    refactor: adiciona struct

--- include/clientes.h
+++ include/clientes.h
@@ -1,10 +1,25 @@
 #ifndef CLIENTES_H
 #define CLIENTES_H
 
+#define MAX_CLIENTES 100
+#define TAM_NOME 100
+#define TAM_CPF 15
+#define TAM_TEL 20
+
+typedef struct {
+
+    int id;
+    char nome[TAM_NOME];
+    char cpf[TAM_CPF];
+    char telefone[TAM_TEL];
+
+} Cliente;
+
 void cadastrarCliente();
 void listarClientes();
-
 void salvarClientes();
 void carregarClientes();
+int menu_Clientes();
+void menuClientes();
 
 #endif

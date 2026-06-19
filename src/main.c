45a62e35ecd6bc24e81bb68c7e388981f1368d7d
Miguel Arcanjo <miguelarcanjomartinsilva@gmail.com>
2026-06-19 11:16:37 -0300

    revert: retorna código sem _Win32

--- src/main.c
+++ src/main.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 #include <locale.h>
-#ifdef _WIN32
-    #include <windows.h>
-#endif
 #include "administradores.h"
 #include "utils.h"
 #include "clientes.h"
@@ -25,12 +22,8 @@ void menuAdministrador();
 
 int main() {
 
-    #indef _WIN32
-        SetConsoleOutputCP(CP_UTF8);
-        SetConsoleCP(CP_UTF8);
-    #else
-        setlocale(LC_ALL, "");
-    #endif
+    setlocale(LC_ALL, "");
+    
     carregarClientes();
     iniciarSistema();
 
